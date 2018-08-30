e95e527c1ed5a3feb59d89f3bc069b7838f4a57b
draichi <lucasdraichi@gmail.com>
2018-08-30 18:29:16 -0300

    plotting

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -2,12 +2,13 @@
   <v-layout>
     <v-flex text-xs-center>
       <img src="/v.png" alt="Vuetify.js" class="mb-5">
-      {{ this.$store.state.ds }}
+      <!-- {{ this.$store.state.ds }} -->
 
       <p>--</p>
-      {{ this.$store.state.y }}
+      <!-- {{ this.$store.state.y }} -->
       <!-- <p v-for="ds in this.$store.state.ds" :key="ds">{{ ds }}</p> -->
       <!-- <p v-for="i in this.$store.state.y" :key="i">{{ i }}</p> -->
+      <vue-plotly :data="data" :layout="layout" :options="options"/>
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
@@ -19,3 +20,20 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import VuePlotly from '@statnett/vue-plotly'
+
+export default {
+  components: {
+    VuePlotly
+  },
+  data: function () {
+    return {
+      data: [{ x: [1, 3], y: [2, 4] }],
+      layout: {},
+      options: {}
+    }
+  }
+}
+</script>
