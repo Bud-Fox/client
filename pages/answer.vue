04aa2dfcf5af7e5b4a7241f367d92cc055c7153a
draichi <lucasdraichi@gmail.com>
2018-09-02 16:39:17 -0300

    date hot fix

--- pages/answer.vue
+++ pages/answer.vue
@@ -2,6 +2,8 @@
   <v-layout>
     <v-flex v-if="this.$store.state.loading"><h1>loading</h1></v-flex>
     <v-flex text-xs-center v-else>
+      {{ ds }}
+      <!-- {{ this.$store.state.forecast }} -->
       <vue-plotly :data="data" :layout="layout" :options="options"/>
       <v-btn
         color="info"
@@ -22,14 +24,26 @@ export default {
     return {
       data: [
         {
-          x: this.$store.state.forecast.ds, y: this.$store.state.forecast.y, name: 'lucas', type: 'scatter'
+          x: this.$store.state.forecast.ds, y: this.$store.state.forecast.y, name: 'price', type: 'scatter'
         },
         {
-          x: this.$store.state.forecast.ds, y: this.$store.state.forecast.yhat, name: 'hat', type: 'scatter'
+          x: this.$store.state.forecast.ds, y: this.$store.state.forecast.yhat, name: 'yhat', type: 'scatter'
+        },
+        {
+          x: this.$store.state.forecast.ds, y: this.$store.state.forecast.trend, name: 'trend', type: 'scatter'
         }
       ],
-      layout: { title: 'lucas', xaxis: { type: 'date' }, yaxis: { title: 'yas' } },
+      layout: { title: 'forecast', xaxis: { type: 'date' }, yaxis: { title: 'Price' } },
       options: {}
+    }
+  },
+  computed: {
+    ds () {
+      for (var i = 0; i < this.$store.state.forecast.ds.length; i++) {
+        var date = new Date(this.$store.state.forecast.ds[i])
+        this.$store.state.forecast.ds[i] = date
+        console.log(this.$store.state.forecast.ds[i])
+      }
     }
   }
 }
